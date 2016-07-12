fe6b7bd78a3de1be6bcf73626ab8fe666c48d87f
italianindy <chayutwork@gmail.com>
2016-07-12 10:47:46 +0700

    sample1
    sample1

---
+++ mainmenu.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="utf-8">
+<title>jQuery Mobile Web App</title>
+<link href="jquery-mobile/jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
+<link href="jquery-mobile/jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="jquery-mobile/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery-mobile/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+</head> 
+<body> 
+
+<div data-role="page" data-theme="d" id="pageMainmenu">
+	<div data-role="header" data-theme="e">
+		<h1><strong>THAI STUDENT CARE</strong></h1>
+	</div>
+	<div data-role="content" >
+    <div align="center">
+      <img src="pic/student2.png" width="120" height="120"> </div>
+       <font size="4"> <ul data-inset="true" data-role="listview" data-theme="e">
+		  <li><a href="http://112.121.150.67/thaistudentcare/popuplogin.php" data-rel="dialog" data-transition="pop">เข้าสู่ระบบ</a></li>
+            <li><a href="http://www.osstec.co.th">เยี่ยมชมเว็บไซต์</a></li>
+			<li><a href="#">ติดต่อเรา</a></li>
+		</ul></font>	
+        
+      
+        	
+	</div>
+	<div data-role="footer" data-theme="e" data-position="fixed">
+		<div data-role="navbar">
+          <ul>
+            <li><a href="http://112.121.150.67/thaistudentcare/mainmenu.php"><font size="4">หน้าหลัก</font></a></li>
+            <li><a href="#"><font size="4">ความคิดเห็น</font></a></li>
+          </ul>
+        </div>
+  </div>
+</div>
+
+
+
+
+ </body>
+</html>
